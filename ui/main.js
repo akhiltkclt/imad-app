efd3b87d0bb50ffbea675d11ea44b6d26f5e9c02
akhiltkclt <36486030+akhiltkclt@users.noreply.github.com>
2018-02-24 22:51:37 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,7 @@ element.innerHTML='This is my new value';
 //move image
 
 var img= document.getElementById('img-id');
+var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+1;
     img.stye.marginLeft=marginLeft+'px';
